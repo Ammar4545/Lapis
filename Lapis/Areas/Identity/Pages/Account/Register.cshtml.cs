584a2dd1feede4aa3fa4e7e388401cc187deec32
Ammar Mahmoud <ammar.m777.2@gmail.com>
2024-01-15 13:41:56 +0200

    updateRegistrationViewWithItslogicInpostMethod

--- Lapis/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Lapis/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Lapis.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -60,6 +61,8 @@ namespace Lapis.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+            public string FullName { get; set; }
+            public string PhoneNumber { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
@@ -74,7 +77,8 @@ namespace Lapis.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email,
+                                                PhoneNumber=Input.PhoneNumber, FullName=Input.FullName };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
--- Lapis/Models/ApplicationUser.cs
+++ Lapis/Models/ApplicationUser.cs
@@ -4,6 +4,6 @@ namespace Lapis.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public string UserName { get; set; }
+        public string FullName { get; set; }
     }
 }
