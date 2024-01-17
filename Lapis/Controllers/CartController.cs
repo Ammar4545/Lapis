7ebe73708831b3810716a9f9f16eb0d6572dd9ec
Ammar Mahmoud <ammar.m777.2@gmail.com>
2024-01-17 08:15:49 +0200

    Remove Action in cart controller

--- Lapis/Controllers/CartController.cs
+++ Lapis/Controllers/CartController.cs
@@ -1,12 +1,14 @@
 ﻿using Lapis.Config;
 using Lapis.Data;
 using Lapis.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Lapis.Controllers
 {
+    [Authorize]
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -26,6 +28,24 @@ namespace Lapis.Controllers
             List<int> shoppingCartProducstsIds = shoppingCartProducts.Select(a => a.ProductId).ToList();
             List<Product> productList = _context.Products.Where(a => shoppingCartProducstsIds.Contains(a.Id)).ToList();
             return View(productList);
+        }
+
+        
+        public IActionResult Remove(int id)
+        {
+            List<ShoppingCart> shoppingCartProducts = new List<ShoppingCart>();
+
+            if (HttpContext.Session.Get<List<ShoppingCart>>(GlobalConst.CartKey) != null
+                && HttpContext.Session.Get<List<ShoppingCart>>(GlobalConst.CartKey).Count > 0)
+            {
+                shoppingCartProducts = HttpContext.Session.Get<List<ShoppingCart>>(GlobalConst.CartKey);
+            }
+
+            shoppingCartProducts.Remove(shoppingCartProducts.FirstOrDefault(a => a.ProductId==id));
+
+            HttpContext.Session.Set(GlobalConst.CartKey, shoppingCartProducts);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
