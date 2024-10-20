23fbc863865bf3425514db1bccb6c37d211217aa
Ammar4545 <ammar.m777.2@gmail.com>
2024-10-20 17:36:52 +0300

    update our front end packages

---
+++ Lapis_DataAcess/Migrations/20240514105957_initiAfterDeleation.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lapis_DataAcess.Migrations
+{
+    public partial class initiAfterDeleation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
