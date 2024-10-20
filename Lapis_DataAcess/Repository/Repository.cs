0dbed7cc4bf5c3f023603e88e6645ff9e2bbb157
Ammar4545 <ammar.m777.2@gmail.com>
2024-10-20 21:16:17 +0300

    add repository class with the implementation of add and find methods

---
+++ Lapis_DataAcess/Repository/Repository.cs
@@ -0,0 +1,52 @@
+﻿using Lapis_DataAcess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lapis_DataAcess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbset;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbset = _db.Set<T>(); 
+
+        }
+        public void Add(T entity)
+        {
+            dbset.Add(entity);
+        }
+
+        public T Find(int id)
+        {
+            return dbset.Find(id);
+        }
+
+        public IEnumerable<T> FirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null, bool isTracking = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null, bool isTracking = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
