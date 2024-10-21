9b8a8a35053b380fe5ac18064ce769b633081076
Ammar4545 <ammar.m777.2@gmail.com>
2024-10-21 11:16:56 +0300

    updating ApplicationTypeController to use it repository

---
+++ Lapis_DataAcess/Repository/ApplicationTypeRepository.cs
@@ -0,0 +1,29 @@
+﻿using Lapis_DataAcess.Repository.IRepository;
+using Lapis_Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lapis_DataAcess.Repository
+{
+    public class ApplicationTypeRepository :Repository<ApplicationType>, IApplicationTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ApplicationTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(ApplicationType applicationType)
+        {
+            var categoryFromDb = _db.ApplicationTypes.FirstOrDefault(x => x.Id == applicationType.Id);
+
+            if (categoryFromDb is not null)
+            {
+                categoryFromDb.Name = applicationType.Name;
+            }
+        }
+    }
+}
---
+++ Lapis_DataAcess/Repository/IRepository/IApplicationTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Lapis_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lapis_DataAcess.Repository.IRepository
+{
+    public interface IApplicationTypeRepository: IRepository<ApplicationType>
+    {
+        void Update(ApplicationType applicationType);
+    }
+}
