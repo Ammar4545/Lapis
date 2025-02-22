23e562347a460edb25172c0d829813de38b50ec9
AmmarM <ammar.m777.2@gmail.com>
2025-02-22 11:29:46 +0200

    return to complete the challenge

---
+++ Lapis_DataAcess/Migrations/20241218144544_BackToFinish.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lapis_DataAcess.Migrations
+{
+    public partial class BackToFinish : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
