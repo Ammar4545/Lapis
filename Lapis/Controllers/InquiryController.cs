472eebd8d33a37a239a69894b76b06a31305e442
AmmarM <ammar.m777.2@gmail.com>
2025-03-14 00:17:16 +0200

    setup InquiryContrller with it viewmodel

---
+++ Lapis/Controllers/InquiryController.cs
@@ -0,0 +1,26 @@
+﻿using Lapis_DataAcess.Repository.IRepository;
+using Lapis_DataAcess;
+using Lapis_Models.ViewModel;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lapis.Controllers
+{
+    public class InquiryController : Controller
+    {
+        
+        private readonly IInquiryDetailRepository _inquiryDetailRepo;
+        private readonly IInquiryHeaderRepository _inquiryHeaderRepo;
+
+        public InquiryController(IInquiryDetailRepository inquiryDetailRepo, IInquiryHeaderRepository inquiryHeaderRepo)
+        {
+            _inquiryDetailRepo = inquiryDetailRepo;
+            _inquiryHeaderRepo = inquiryHeaderRepo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lapis_Models/ViewModel/InquiruVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lapis_Models.ViewModel
+{
+    public class InquiruVM
+    {
+        public InquiryHeaders inquiryHeader { get; set; }
+        public IEnumerable<InquiryDetails> inquiryDetails { get; set; }
+    }
+}
